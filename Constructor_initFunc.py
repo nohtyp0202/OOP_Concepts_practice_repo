946c9d01ccda3d08b6cccb130f383832deef0509
nohtyp0202 <accutesting19@gmail.com>
2020-06-12 17:30:06 -0500

    1. This code has a class Parrot with inti functiona and other functions defined in it.It also has class variable. 2. ObjectCreation.py - This code has a object that is created. 3. Constructor_initFunc.py - Uses special __init__ function to initialize the attributes

---
+++ Constructor_initFunc.py
@@ -0,0 +1,20 @@
+class ComplexNumber:
+    def __init__(self,r = 0,i = 0 ):
+        self.real = r
+        self.img = i
+    def get_data(self):
+        print self.real , self.img
+
+# creating a complex number by call class method
+# create an instance of class or object
+num1 = ComplexNumber(2,3)
+num1.get_data()
+
+num2 = ComplexNumber(5)
+num2.get_data()
+
+#creating an attribute
+num2.attr = 10
+print num2.real,num2.img,num2.attr
+
+
