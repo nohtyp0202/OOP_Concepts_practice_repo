284528e0f65990e4e7bfb8bd268f3a69dc3072e9
nohtyp0202 <accutesting19@gmail.com>
2020-06-20 16:28:50 -0500

    1. This code has a class Parrot with inti functiona and other functions defined in it.It also has class variable. 2. ObjectCreation.py - This code has a object that is created. 3. Constructor_initFunc.py - Uses special __init__ function to initialize the attributes 4. Constructor_initFunc.py - deleting of attibute of an object and deleting the object itself 5.Inheritence 6.Multiple inheritance - init in derived class updated 7. Multi level inheritance - init in derived class  updated

---
+++ Polymorphism.py
@@ -0,0 +1,9 @@
+
+# polymorphism in python
+# same len() gives len but for different datatypes like string, list and dictionary
+# same function /method but used for different data types is a type of polymorphism
+print len("polymorphism")
+
+print len(["polymorphism", "Inheritance","Abstraction"])
+
+print len({"Double" : "twice", "Triple" : "Trice"})
