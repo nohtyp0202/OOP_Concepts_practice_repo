842429104bd1caea79889c6c0a37f3db06b29421
nohtyp0202 <accutesting19@gmail.com>
2020-06-20 17:00:56 -0500

    1. This code has a class Parrot with inti functiona and other functions defined in it.It also has class variable. 2. ObjectCreation.py - This code has a object that is created. 3. Constructor_initFunc.py - Uses special __init__ function to initialize the attributes 4. Constructor_initFunc.py - deleting of attibute of an object and deleting the object itself 5.Inheritence 6.Multiple inheritance - init in derived class updated 7. Multi level inheritance - init in derived class  updated
    8. Polymorphism

--- Polymorphism.py
+++ Polymorphism.py
@@ -7,3 +7,28 @@ print len("polymorphism")
 print len(["polymorphism", "Inheritance","Abstraction"])
 
 print len({"Double" : "twice", "Triple" : "Trice"})
+
+
+# Same method names are used in two different classes
+# these two classes are not connected to each other
+class Rose():
+    def color(self):
+        print "Roses are RED is color"
+    def Grown(self):
+        print "Roses grown on land.."
+
+class Lotus():
+    def color(self):
+        print "Lotus are PINK in color"
+
+    def Grown(self):
+        print "Lotus grown on water.."
+
+Flower_rose = Rose()
+Flower_lotus = Lotus()
+
+# Because of polymorphism , we can access each method
+# inside the two classed by itereating through the tuple of class objects
+for flowers in (Flower_rose,Flower_lotus):
+    flowers.color()
+    flowers.Grown()
